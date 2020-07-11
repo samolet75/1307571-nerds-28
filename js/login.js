8c132e55e81ed5e48869fb44e8ae0f0a59497922
samolet75 <60198055+samolet75@users.noreply.github.com>
2020-07-11 12:20:50 +0300

    js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,49 @@
-var loginLink = document.querySelector(".login-link");
+var Buttonmail = document.querySelector(".button-mail");
 var loginPopup = document.querySelector(".modal-login");
-loginLink.addEventListener("click", function (evt) {
+var loginClose = loginPopup.querySelector(".modal-close");
+var loginForm = loginPopup.querySelector(".login-form");
+var loginLogin = loginPopup.querySelector(".login-icon-name");
+var loginPassword = loginPopup.querySelector(".login-icon-mail");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+Buttonmail.addEventListener("click", function (evt) {
     evt.preventDefault();
     loginPopup.classList.add("modal-show");
+
+    if (storage) {
+    loginLogin.value = storage;
+    loginPassword.focus();
+  } else {
+    loginLogin.focus();
+  }
+});
+
+  loginClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-show");
   });
+
+  loginForm.addEventListener("submit", function (evt) {
+    if (!loginLogin.value || !loginPassword.value) {
+    evt.preventDefault();
+} else {
+  localStorage.setItem("login", loginLogin.value);
+}
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+    }
+  }
+});
